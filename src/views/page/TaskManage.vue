167dd6a0abf5498fe79b15c9a1789c49d2ac85ea
OrangeGuo <1772086465@qq.com>
2018-10-23 16:16:55 +0800

    add taskmanage module

---
+++ src/views/page/TaskManage.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-table
+        :data="tableData3"
+        height="250"
+        border
+        style="width: 100%">
+        <el-table-column
+            prop="taskName"
+            label="任务名称"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="date"
+            label="创建时间"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="detail"
+            label="备注">
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    export default {
+        name: "TaskManage"
+    }
+</script>
+
+<style scoped>
+
+</style>
