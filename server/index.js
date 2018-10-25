75c5c98286b95039439149b86750ad51d3efc32d
kirua1996 <a13276635022@163.com>
2018-10-25 22:02:49 +0800

    modules sql

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const userApi = require('./api/userApi')
 const taskApi = require('./api/taskApi')
+const modulesApi=require('./api/modulesApi')
 const fs = require('fs')
 const path = require('path')
 const bodyParser = require('body-parser')
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}))//解析表单不含文件
 // 后端api路由
 app.use('/api/user', userApi)
 app.use('/api/task', taskApi)
+app.use('/api/modules',modulesApi)
 
 // 监听端口
 app.listen(3000)
