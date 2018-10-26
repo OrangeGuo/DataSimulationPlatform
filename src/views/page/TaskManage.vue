7786b4c1530ec275c8178816836d990af9b667e3
OrangeGuo <1772086465@qq.com>
2018-10-26 19:14:38 +0800

    remove tree demon

--- src/views/page/TaskManage.vue
+++ src/views/page/TaskManage.vue
@@ -53,36 +53,7 @@
                 </template>
             </el-table-column>
         </el-table>
-        <div class="custom-tree-container">
 
-            <div class="block">
-                <p>使用 scoped slot</p>
-                <el-tree
-                    :data="data5"
-
-                    node-key="id"
-                    default-expand-all
-                    :expand-on-click-node="true">
-      <span class="custom-tree-node" slot-scope="{ node, data }">
-        <span>{{ node.label }}</span>
-        <span>
-          <el-button
-              type="text"
-              size="mini"
-              @click="() => append(data)">
-            Append
-          </el-button>
-          <el-button
-              type="text"
-              size="mini"
-              @click="() => remove(node, data)">
-            Delete
-          </el-button>
-        </span>
-      </span>
-                </el-tree>
-            </div>
-        </div>
 
         <el-dialog title="新建任务" :visible.sync="dialogAddForm" width="30%">
             <el-form :model="form">
@@ -119,45 +90,12 @@
     export default {
         name: "TaskManage",
 
-  id : 1000,
+
         data() {
-            const data = [{
-                id: 1,
-                label: '一级 1',
-                children: [{
-                    id: 4,
-                    label: '二级 1-1',
-                    children: [{
-                        id: 9,
-                        label: '三级 1-1-1'
-                    }, {
-                        id: 10,
-                        label: '三级 1-1-2'
-                    }]
-                }]
-            }, {
-                id: 2,
-                label: '一级 2',
-                children: [{
-                    id: 5,
-                    label: '二级 2-1'
-                }, {
-                    id: 6,
-                    label: '二级 2-2'
-                }]
-            }, {
-                id: 3,
-                label: '一级 3',
-                children: [{
-                    id: 7,
-                    label: '二级 3-1'
-                }, {
-                    id: 8,
-                    label: '二级 3-2'
-                }]
-            }];
+
+
             return {
-                data5: JSON.parse(JSON.stringify(data)),
+
                 formLabelWidth: '120px',
                 form: {
                     taskName: '',
@@ -283,35 +221,11 @@
         mounted() {
             this.listTask();
         },
-        append(data) {
-            const newChild = {id: id++, label: 'testtest', children: []};
-            if (!data.children) {
-                this.$set(data, 'children', []);
-            }
-            data.children.push(newChild);
-        },
 
-        remove(node, data) {
-            const parent = node.parent;
-            const children = parent.data.children || parent.data;
-            const index = children.findIndex(d => d.id === data.id);
-            children.splice(index, 1);
-        },
-
-        renderContent(h, {node, data, store}) {
-
-        }
 
     }
 </script>
 
 <style scoped>
-  .custom-tree-node {
-    flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 14px;
-    padding-right: 8px;
-  }
+
 </style>
