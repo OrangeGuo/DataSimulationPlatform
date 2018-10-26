5c6766a700824593a387f35fe70bbc57f5371d41
OrangeGuo <1772086465@qq.com>
2018-10-26 15:16:03 +0800

    tree node

--- src/views/page/TaskManage.vue
+++ src/views/page/TaskManage.vue
@@ -54,25 +54,15 @@
             </el-table-column>
         </el-table>
         <div class="custom-tree-container">
-            <div class="block">
-                <p>使用 render-content</p>
-                <el-tree
-                    :data="data4"
-                    show-checkbox
-                    node-key="id"
-                    default-expand-all
-                    :expand-on-click-node="false"
-                    :render-content="renderContent">
-                </el-tree>
-            </div>
+
             <div class="block">
                 <p>使用 scoped slot</p>
                 <el-tree
                     :data="data5"
-                    show-checkbox
+
                     node-key="id"
                     default-expand-all
-                    :expand-on-click-node="false">
+                    :expand-on-click-node="true">
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
         <span>
@@ -129,44 +119,45 @@
     export default {
         name: "TaskManage",
 
-
+  id : 1000,
         data() {
+            const data = [{
+                id: 1,
+                label: '一级 1',
+                children: [{
+                    id: 4,
+                    label: '二级 1-1',
+                    children: [{
+                        id: 9,
+                        label: '三级 1-1-1'
+                    }, {
+                        id: 10,
+                        label: '三级 1-1-2'
+                    }]
+                }]
+            }, {
+                id: 2,
+                label: '一级 2',
+                children: [{
+                    id: 5,
+                    label: '二级 2-1'
+                }, {
+                    id: 6,
+                    label: '二级 2-2'
+                }]
+            }, {
+                id: 3,
+                label: '一级 3',
+                children: [{
+                    id: 7,
+                    label: '二级 3-1'
+                }, {
+                    id: 8,
+                    label: '二级 3-2'
+                }]
+            }];
             return {
-                   const data = [{
-        id: 1,
-        label: '一级 1',
-        children: [{
-          id: 4,
-          label: '二级 1-1',
-          children: [{
-            id: 9,
-            label: '三级 1-1-1'
-          }, {
-            id: 10,
-            label: '三级 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: '一级 2',
-        children: [{
-          id: 5,
-          label: '二级 2-1'
-        }, {
-          id: 6,
-          label: '二级 2-2'
-        }]
-      }, {
-        id: 3,
-        label: '一级 3',
-        children: [{
-          id: 7,
-          label: '二级 3-1'
-        }, {
-          id: 8,
-          label: '二级 3-2'
-        }]
-      }],
+                data5: JSON.parse(JSON.stringify(data)),
                 formLabelWidth: '120px',
                 form: {
                     taskName: '',
@@ -292,35 +283,35 @@
         mounted() {
             this.listTask();
         },
-      append(data) {
-        const newChild = { id: id++, label: 'testtest', children: [] };
-        if (!data.children) {
-          this.$set(data, 'children', []);
+        append(data) {
+            const newChild = {id: id++, label: 'testtest', children: []};
+            if (!data.children) {
+                this.$set(data, 'children', []);
+            }
+            data.children.push(newChild);
+        },
+
+        remove(node, data) {
+            const parent = node.parent;
+            const children = parent.data.children || parent.data;
+            const index = children.findIndex(d => d.id === data.id);
+            children.splice(index, 1);
+        },
+
+        renderContent(h, {node, data, store}) {
+
         }
-        data.children.push(newChild);
-      },
 
-      remove(node, data) {
-        const parent = node.parent;
-        const children = parent.data.children || parent.data;
-        const index = children.findIndex(d => d.id === data.id);
-        children.splice(index, 1);
-      },
-
-      renderContent(h, { node, data, store }) {
-        return (
-          <span class="custom-tree-node">
-            <span>{node.label}</span>
-            <span>
-              <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
-              <el-button size="mini" type="text" on-click={ () => this.remove(node, data) }>Delete</el-button>
-            </span>
-          </span>);
-      }
-    }
     }
 </script>
 
 <style scoped>
-
+  .custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
 </style>
