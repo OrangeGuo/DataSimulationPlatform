2aa64fb65e68ff9beb690399f45bf26a5c8c5f80
OrangeGuo <1772086465@qq.com>
2018-11-15 14:04:22 +0800

    调整下拉菜单样式和位置

--- src/components/page/SideBar.vue
+++ src/components/page/SideBar.vue
@@ -1,15 +1,6 @@
 <template>
 	<div class="side-bar" >
-		<el-dropdown style="margin-left: 120px;margin-top: 50px" @command="handleCommand">
-                <span class="el-dropdown-link" >
-                    类别<i class="el-icon-arrow-down el-icon--right"></i>
-                </span>
-                <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item command="1">人文</el-dropdown-item>
-                    <el-dropdown-item command="2">地理</el-dropdown-item>
-                    <el-dropdown-item command="4">小说</el-dropdown-item>
-                </el-dropdown-menu>
-        </el-dropdown>
+
 	</div>
 </template>
 
@@ -22,9 +13,7 @@
 		},
 
 		methods:{
-            handleCommand(command) {
-                this.$message('click on item ' + command);
-             }
+
 		}
 	}
 </script>
@@ -40,11 +29,5 @@
 	.side-bar ul{
 		height:100%;
 	}
-    .el-dropdown-link {
-    cursor: pointer;
-    color: #409EFF;
-  }
-  .el-icon-arrow-down {
-    font-size: 12px;
-  }
+
 </style>
--- src/views/page/TaskManage.vue
+++ src/views/page/TaskManage.vue
@@ -2,7 +2,18 @@
 
     <div>
         <div style="height: 50px">
-            <el-input style="Float: left;height:50px;width: 500px;" float="left" placeholder="请输入关键字"
+            <el-dropdown @command="handleCommand">
+            <span class="el-dropdown-link">
+                {{dropItem}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="科学">科学</el-dropdown-item>
+                    <el-dropdown-item command="自然">自然</el-dropdown-item>
+                    <el-dropdown-item command="文化">文化</el-dropdown-item>
+                    <el-dropdown-item command="全部">全部</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+            <el-input style="Float: left;height:50px;width: 500px;" float="left" placeholder="请输入书名、作者"
                       clearable></el-input>
             <el-button style="Float: left;height:40px;" type="primary" @click="" icon="el-icon-search">搜索
             </el-button>
@@ -14,19 +25,15 @@
             border
             style="width: 80%"
             highlight-current-row
-
         >
             <el-table-column
                 prop="name"
                 label="图书名称"
                 width="200%">
                 <template slot-scope="scope">
-
-
                     <div slot="reference" class="name-wrapper">
                         <el-tag size="medium">{{ scope.row.name }}</el-tag>
                     </div>
-
                 </template>
             </el-table-column>
             <el-table-column
@@ -39,7 +46,6 @@
                     <span style="margin-left: 10px">{{ scope.row.writer }}</span>
                 </template>
             </el-table-column>
-
             <el-table-column
                 prop="resbooks"
                 label="可借数量"
@@ -50,13 +56,12 @@
             </el-table-column>
             <el-table-column
 
-                label="操作"
+                label="状态"
                 width="150">
                 <template slot-scope="scope">
-                    <el-button icon="el-icon-delete" type="danger"
-                               size="small"></el-button>
-                    <el-button icon="el-icon-edit" type="primary" size="small"
-                    ></el-button>
+                    <el-tooltip content="可借阅" placement="top">
+                        <el-button icon="el-icon-plus" type="primary" size="medium"></el-button>
+                    </el-tooltip>
                 </template>
             </el-table-column>
         </el-table>
@@ -67,58 +72,39 @@
     export default {
         name: "TaskManage",
 
-
         data() {
-
-
             return {
-
-
-
+                dropItem: "全部",
                 filterText: "",
                 prop: {
                     label: 'name',
                     children: 'zones',
                     isLeaf: 'leaf'
                 },
-
-
                 tableData: []
             }
         },
         methods: {
-
-
+            handleCommand(command) {
+                this.dropItem=command;
+                this.$message('调整类别为' + command);
+            },
             listTask() {
                 const self = this;
                 self.$axios.post('/api/books/listBook').then((res) => {
                     self.tableData = [];
                     res.data.some(item => {
-
-
                         self.tableData.push({
                             name: item.bookname,
                             writer: item.writer,
                             resbooks: item.resbooks,
                             findNumber: item.findNumber
                         })
-                        console.log(self.flag);
-
                     });
-
-                }).then(() => {
-                    if (self.flag === 0) {
-                        if (self.tableData.length > 0) {
-
-                            self.current_task = self.tableData[0].id;
-                        }
-                        self.flag = 1;
-                    }
                 });
             },
             getDate(day) {
                 day = day.split("-");
-
                 let dateyear = day[0];
                 let datemonth = day[1];
                 day = day[2].split("T")
@@ -134,15 +120,10 @@
                 datehour = String(datehour);
                 return dateyear + '-' + datemonth + '-' + dateday + ' ' + datehour + ':' + datemin + ':' + datesec;
             },
-
-
         },
-
         mounted() {
             this.listTask();
         },
-
-
     }
 </script>
 
