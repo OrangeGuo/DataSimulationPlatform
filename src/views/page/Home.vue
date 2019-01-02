677a0838a00d5eeb9b839e6397cec7291b7ae7f6
OrangeGuo <1772086465@qq.com>
2019-01-02 13:11:50 +0800

    stage

--- src/views/page/Home.vue
+++ src/views/page/Home.vue
@@ -1,16 +1,30 @@
 <template>
-    <el-steps :active="2" align-center>
-        <el-step title="步骤1" description="新建任务"></el-step>
-        <el-step title="步骤2" description="配置任务模型"></el-step>
-        <el-step title="步骤3" description="进行数据绑定"></el-step>
-        <el-step title="步骤4" description="选择算法分析"></el-step>
-        <el-step title="步骤5" description="查看分析结果"></el-step>
-    </el-steps>
+    <div>
+        <el-steps :active="active" align-center finish-status="success">
+            <el-step title="步骤1" description="新建任务"></el-step>
+            <el-step title="步骤2" description="配置任务模型"></el-step>
+            <el-step title="步骤3" description="进行数据绑定"></el-step>
+            <el-step title="步骤4" description="选择算法分析"></el-step>
+            <el-step title="步骤5" description="查看分析结果"></el-step>
+        </el-steps>
+        <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        data() {
+            return {
+                active: 0
+            };
+        },
+
+        methods: {
+            next() {
+                if (this.active++ > 5) this.active = 0;
+            }
+        }
     }
 </script>
 
